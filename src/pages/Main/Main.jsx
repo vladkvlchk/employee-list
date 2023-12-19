eb3f8909b690672c345669c1ba50d5c8eb68d2fe
Vladyslav <prostoandrei9@gmail.com>
2023-12-19 22:47:57 +0200

    phone validation

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MyButton from "../../components/MyButton/MyButton";
 import axios from "../../axios";
 import styles from "./Main.module.scss";
@@ -9,13 +9,20 @@ import {
   FormControlLabel,
   Radio,
   FormLabel,
-  Button,
 } from "@mui/material";
 
 const Main = () => {
-  const [employees, setEmployees] = React.useState([]);
-  const [page, setPage] = React.useState(1);
-  const [totalPages, setTotalPages] = React.useState(2);
+  const [employees, setEmployees] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(2);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isValidEmail, setValidEmail] = useState(true);
+  const [phone, setPhone] = useState("");
+  const [isValidPhone, setValidPhone] = useState(true);
+  const [position, setPosition] = useState("");
+  const [file, setFile] = useState(null);
+  const [positions, setPositions] = useState([]);
 
   const getTeam = async () => {
     try {
@@ -35,15 +42,76 @@ const Main = () => {
     }
   };
 
+  const getPositions = async () => {
+    try {
+      const { data } = await axios.get("/positions");
+      setPositions(data.positions);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   const onShowMore = () => {
     console.log("click");
     getTeam();
     console.log(page);
   };
 
-  // React.useEffect(() => {
-  //   getTeam();
-  // }, []);
+  const validateEmail = (input) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(input);
+  };
+
+  const validatePhone = (input) => {
+    const phoneRegex = /^\+38 \(\d{3}\) \d{3} - \d{2} - \d{2}$/;
+    return phoneRegex.test(input);
+  };
+
+  const onChangePhone = (e) => {
+    const newValue = e.target.value;
+
+    if (newValue.length < phone.length) {
+      setPhone(newValue);
+    } else if (/^[0-9]+$/.test(newValue.slice(-1))){
+      if (newValue.length === 1) {
+        setPhone("+38 (" + e.target.value);
+      } else if (newValue.length === 8) {
+        setPhone(e.target.value + ") ");
+      } else if (newValue.length === 13 || newValue.length === 18) {
+        setPhone(e.target.value + " - ");
+      } else if (newValue.length < 24) {
+        setPhone(newValue);
+      }
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    setValidEmail(validateEmail(email));
+    setValidPhone(validatePhone(phone));
+
+    if (
+      isValidEmail &&
+      isValidPhone &&
+      name.length > 1 &&
+      name.length < 61 &&
+      file
+    ) {
+      console.log("sending request...");
+      const { data } = axios.post("/users", {
+        body: { name, email, phone: phone.replace(" ", "").replace("-", "") },
+      });
+    }
+
+    console.log({ name, email, phone, position });
+  };
+
+  useEffect(() => {
+    // getTeam();
+    getPositions();
+    setPosition(positions[0]);
+  }, []);
 
   return (
     <main>
@@ -82,18 +150,26 @@ const Main = () => {
       </section>
       <section className={styles.formSection}>
         <h1>Working with POST request</h1>
-        <form>
+        <form onSubmit={onSubmit}>
           <TextField
             className={styles.inputText}
             id="outlined-basic"
             label="Your name"
             variant="outlined"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
           />
           <TextField
             className={styles.inputText}
             id="outlined-basic"
             label="Email"
             variant="outlined"
+            onChange={(e) => setEmail(e.target.value)}
+            error={!isValidEmail}
+            value={email}
+            helperText={
+              !isValidEmail ? "Please enter a valid email address" : ""
+            }
           />
           <TextField
             className={styles.inputText}
@@ -101,6 +177,9 @@ const Main = () => {
             label="Phone"
             variant="outlined"
             helperText="+38 (XXX) XXX - XX - XX"
+            onChange={onChangePhone}
+            value={phone}
+            error={!isValidPhone}
           />
           <FormLabel id="demo-radio-buttons-group-label">
             Select your position
@@ -109,62 +188,30 @@ const Main = () => {
             aria-labelledby="demo-radio-buttons-group-label"
             defaultValue="female"
             name="radio-buttons-group"
+            value={position}
           >
-            <FormControlLabel
-              value="Frontend developer"
-              control={
-                <Radio
-                  sx={{
-                    "&.Mui-checked": {
-                      color: "rgba(0, 189, 211, 1)",
-                    },
-                  }}
-                />
-              }
-              label="Frontend developer"
-            />
-            <FormControlLabel
-              value="Backend developer"
-              control={
-                <Radio
-                  sx={{
-                    "&.Mui-checked": {
-                      color: "rgba(0, 189, 211, 1)",
-                    },
-                  }}
-                />
-              }
-              label="Backend developer"
-            />
-            <FormControlLabel
-              value="Designer"
-              control={
-                <Radio
-                  sx={{
-                    "&.Mui-checked": {
-                      color: "rgba(0, 189, 211, 1)",
-                    },
-                  }}
-                />
-              }
-              label="Designer"
-            />
-            <FormControlLabel
-              value="QA"
-              control={
-                <Radio
-                  sx={{
-                    "&.Mui-checked": {
-                      color: "rgba(0, 189, 211, 1)",
-                    },
-                  }}
-                />
-              }
-              label="QA"
-            />
+            {positions.map((position) => (
+              <FormControlLabel
+                key={position.id}
+                value={position.name}
+                control={
+                  <Radio
+                    sx={{
+                      "&.Mui-checked": {
+                        color: "rgba(0, 189, 211, 1)",
+                      },
+                    }}
+                  />
+                }
+                label={position.name}
+                onChange={(e) => setPosition(e.target.value)}
+              />
+            ))}
           </RadioGroup>
-          <input type="file" />
-          <MyButton disabled>Sign Up</MyButton>
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+          <MyButton disabled={!name || !email || !phone || !file} type="submit">
+            Sign Up
+          </MyButton>
         </form>
       </section>
     </main>
