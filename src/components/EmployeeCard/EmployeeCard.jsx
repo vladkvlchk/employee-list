c1bf86af6dc935c8e7f25d248155667e2dc0a514
Vladyslav <prostoandrei9@gmail.com>
2023-12-21 15:30:43 +0200

    default avatar

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -1,13 +1,14 @@
 import styles from "./EmployeeCard.module.scss";
+import avatar from "../../assents/avatar"
 
 const EmployeeCard = ({ email, photo, name, position, phone }) => {
   return (
     <div className={styles.card}>
       <picture className={styles.profilePhoto}>
-        <img src={photo} alt="profile photo" />
+        {photo ? <img src={photo} alt="profile photo" /> : avatar}
       </picture>
       <p>{name}</p>
-      <div>
+      <div className={styles.aboutMember}>
         <p>{position}</p>
         <p>{email}</p>
         <p>{`${phone.slice(0,3)} (${phone.slice(3,6)}) ${phone.slice(6,9)} ${phone.slice(9,11)} ${phone.slice(11)}`}</p>
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -12,7 +12,6 @@ import {
 } from "@mui/material";
 
 const Main = () => {
-
   const [employees, setEmployees] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(2);
@@ -24,7 +23,7 @@ const Main = () => {
   const [position, setPosition] = useState("");
   const [file, setFile] = useState(null);
   const [positions, setPositions] = useState([]);
-  
+
   const getTeam = async () => {
     try {
       const { data } = await axios.get("/users", {
@@ -33,7 +32,7 @@ const Main = () => {
           count: 6,
         },
       });
-      
+
       console.log(data);
       setEmployees((prev) => [...prev, ...data.users]);
       setPage(page + 1);
@@ -42,35 +41,36 @@ const Main = () => {
       console.error(err);
     }
   };
-  
+
   const getPositions = async () => {
     try {
       const { data } = await axios.get("/positions");
       setPositions(data.positions);
+      setPosition(data.positions[0].name);
     } catch (e) {
       console.log(e);
     }
   };
-  
+
   const onShowMore = () => {
     console.log("click");
     getTeam();
     console.log(page);
   };
-  
+
   const validateEmail = (input) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(input);
   };
-  
+
   const validatePhone = (input) => {
     const phoneRegex = /^\+38 \(\d{3}\) \d{3} - \d{2} - \d{2}$/;
     return phoneRegex.test(input);
   };
-  
+
   const onChangePhone = (e) => {
     const newValue = e.target.value;
-    
+
     if (newValue.length < phone.length) {
       setPhone(newValue);
     } else if (newValue.length === 1) {
@@ -82,136 +82,138 @@ const Main = () => {
     } else if (newValue.length < 24) {
       setPhone(newValue);
     }
+    
   };
 
-const onSubmit = (event) => {
-  event.preventDefault();
+  const onSubmit = (event) => {
+    event.preventDefault();
 
-  setValidEmail(validateEmail(email));
-  setValidPhone(validatePhone(phone));
+    setValidEmail(validateEmail(email));
+    setValidPhone(validatePhone(phone));
 
-  if (
-    isValidEmail &&
-    isValidPhone &&
-    name.length > 1 &&
-    name.length < 61 &&
-    file
-  ) {
-    console.log("sending request...");
-    const { data } = axios.post("/users", {
-      body: { name, email, phone: phone.replace(" ", "").replace("-", "") },
-    });
-  }
+    if (
+      isValidEmail &&
+      isValidPhone &&
+      name.length > 1 &&
+      name.length < 61 &&
+      file
+    ) {
+      console.log("sending request...");
+      const { data } = axios.post("/users", {
+        body: { name, email, phone: phone.replace(" ", "").replace("-", "") },
+      });
+    }
 
-  console.log({ name, email, phone, position });
-};
+    console.log({ name, email, phone, position });
+  };
 
-useEffect(() => {
-  // getTeam();
-  getPositions();
-  setPosition(positions[0]);
-}, []);
+  useEffect(() => {
+    getTeam();
+    getPositions();
+  }, []);
 
-return (
-  <main>
-    <section className={styles.welcomeSection}>
-      <div className={styles.welcomeContainer}>
-        <h1>Test assignment for front-end developer</h1>
-        <p>
-          What defines a good front-end developer is one that has skilled
-          knowledge of HTML, CSS, JS with a vast understanding of User design
-          thinking as they'll be building web interfaces with accessibility in
-          mind. They should also be excited to learn, as the world of Front-End
-          Development keeps evolving.
-        </p>
-        <MyButton>Sign Up</MyButton>
-      </div>
-    </section>
-    <section className={styles.teamSection}>
-      <h1>Working with GET request</h1>
-      <div className={styles.employeeList}>
-        {employees.map((employee) => (
-          <EmployeeCard
-            key={employee.id}
-            name={employee.name}
-            photo={employee.photo}
-            email={employee.email}
-            position={employee.position}
-            phone={employee.phone}
-          />
-        ))}
-      </div>
-      {page <= totalPages ? (
-        <MyButton onClick={onShowMore}>Show more</MyButton>
-      ) : (
-        <></>
-      )}
-    </section>
-    <section className={styles.formSection}>
-      <h1>Working with POST request</h1>
-      <form onSubmit={onSubmit}>
-        <TextField
-          className={styles.inputText}
-          id="outlined-basic"
-          label="Your name"
-          variant="outlined"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <TextField
-          className={styles.inputText}
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          onChange={(e) => setEmail(e.target.value)}
-          error={!isValidEmail}
-          value={email}
-          helperText={!isValidEmail ? "Please enter a valid email address" : ""}
-        />
-        <TextField
-          className={styles.inputText}
-          id="outlined-basic"
-          label="Phone"
-          variant="outlined"
-          helperText="+38 (XXX) XXX - XX - XX"
-          onChange={onChangePhone}
-          value={phone}
-          error={!isValidPhone}
-        />
-        <FormLabel id="demo-radio-buttons-group-label">
-          Select your position
-        </FormLabel>
-        <RadioGroup
-          aria-labelledby="demo-radio-buttons-group-label"
-          defaultValue="female"
-          name="radio-buttons-group"
-          value={position}
-        >
-          {positions.map((position) => (
-            <FormControlLabel
-              key={position.id}
-              value={position.name}
-              control={
-                <Radio
-                  sx={{
-                    "&.Mui-checked": {
-                      color: "rgba(0, 189, 211, 1)",
-                    },
-                  }}
-                />
-              }
-              label={position.name}
-              onChange={(e) => setPosition(e.target.value)}
+  return (
+    <main>
+      <section className={styles.welcomeSection}>
+        <div className={styles.welcomeContainer}>
+          <h1>Test assignment for front-end developer</h1>
+          <p>
+            What defines a good front-end developer is one that has skilled
+            knowledge of HTML, CSS, JS with a vast understanding of User design
+            thinking as they'll be building web interfaces with accessibility in
+            mind. They should also be excited to learn, as the world of
+            Front-End Development keeps evolving.
+          </p>
+          <MyButton>Sign Up</MyButton>
+        </div>
+      </section>
+      <section className={styles.teamSection}>
+        <h1>Working with GET request</h1>
+        <div className={styles.employeeList}>
+          {employees.map((employee) => (
+            <EmployeeCard
+              key={employee.id}
+              name={employee.name}
+              photo={employee.photo}
+              email={employee.email}
+              position={employee.position}
+              phone={employee.phone}
             />
           ))}
-        </RadioGroup>
-        <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-        <MyButton disabled={!name || !email || !phone || !file} type="submit">
-          Sign Up
-        </MyButton>
-      </form>
-    </section>
-  </main>
+        </div>
+        {page <= totalPages ? (
+          <MyButton onClick={onShowMore}>Show more</MyButton>
+        ) : (
+          <></>
+        )}
+      </section>
+      <section className={styles.formSection}>
+        <h1>Working with POST request</h1>
+        <form onSubmit={onSubmit}>
+          <TextField
+            className={styles.inputText}
+            id="outlined-basic"
+            label="Your name"
+            variant="outlined"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <TextField
+            className={styles.inputText}
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            onChange={(e) => setEmail(e.target.value)}
+            error={!isValidEmail}
+            value={email}
+            helperText={
+              !isValidEmail ? "Please enter a valid email address" : ""
+            }
+          />
+          <TextField
+            className={styles.inputText}
+            id="outlined-basic"
+            label="Phone"
+            variant="outlined"
+            helperText="+38 (XXX) XXX - XX - XX"
+            onChange={onChangePhone}
+            value={phone}
+            error={!isValidPhone}
+          />
+          <FormLabel id="demo-radio-buttons-group-label">
+            Select your position
+          </FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+            value={position}
+          >
+            {positions.map((position) => (
+              <FormControlLabel
+                key={position.id}
+                value={position.name}
+                control={
+                  <Radio
+                    sx={{
+                      "&.Mui-checked": {
+                        color: "rgba(0, 189, 211, 1)",
+                      },
+                    }}
+                  />
+                }
+                label={position.name}
+                onChange={(e) => setPosition(e.target.value)}
+              />
+            ))}
+          </RadioGroup>
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+          <MyButton disabled={!name || !email || !phone || !file} type="submit">
+            Sign Up
+          </MyButton>
+        </form>
+      </section>
+    </main>
   );
 };
 
